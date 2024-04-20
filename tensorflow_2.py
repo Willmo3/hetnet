f0952555f7bae1cc418dc5f27cf419b2c2a78e6e
William Morris <morri2wj@dukes.jmu.edu>
2024-04-20 08:08:45 -0400

    Add tf2 model prep

--- tensorflow_2.py
+++ tensorflow_2.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
 import matplotlib.pyplot as plt
+
+from keras import models, layers
 from keras.datasets import cifar10
 
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
@@ -26,8 +27,31 @@ def visualize(train_images, train_labels):
     plt.show()
 
 
+def prepare_model():
+    """
+    Prepare a standard 3-conv layer model..
+    :return: the model
+    """
+    model = models.Sequential()
+    
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(10))
+
+    return model
+
+
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 # Normalizing pixel values to be between 0, 1
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 visualize(x_train, y_train)
+model = prepare_model()
+model.summary()
+
+
