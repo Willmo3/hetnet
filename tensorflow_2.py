fba1cf5da996bf2201f0b2c34c5e5786a38a266c
William Morris <morri2wj@dukes.jmu.edu>
2024-04-20 12:22:39 -0400

    Remove redundant code; add explicit fit field vars

--- tensorflow_2.py
+++ tensorflow_2.py
@@ -1,32 +1,6 @@
-import matplotlib.pyplot as plt
-
 import tensorflow as tf
 
 from tensorflow.keras import datasets, layers, models
-
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck']
-
-
-def visualize(train_images, train_labels):
-    """
-    Visualize the first 25 images of the cifar image set.
-
-    :param train_images: Images to visualize
-    :param train_labels: Provided class labels for images
-    """
-    plt.figure(figsize=(10, 10))
-    for i in range(25):
-        plt.subplot(5, 5, i + 1)
-        plt.xticks([])
-        plt.yticks([])
-        plt.grid(False)
-        plt.imshow(train_images[i])
-        # The CIFAR labels happen to be arrays,
-        # which is why you need the extra index
-        plt.xlabel(class_names[train_labels[i][0]])
-    plt.show()
-
 
 def prepare_model():
     """
@@ -46,16 +20,21 @@ def prepare_model():
 
     return retval
 
+# ***** LOAD DATA ***** #
 
+# Using provided cifar10 data from keras dataset
 (x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
 # Normalizing pixel values to be between 0, 1
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-visualize(x_train, y_train)
 model = prepare_model()
-model.summary()
+# Set model parameters
+# Note: in tensorflow, explicitly set as fields to model.fit
+optimizer='adam'
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+epochs = 10
+batch_size = 128
 
-model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-              , metrics=['accuracy'])
-history = model.fit(x_train, y_train, epochs=10,
-                    validation_data=(x_test, y_test))
+model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+model.fit(x_train, y_train,
+          epochs=epochs, batch_size=batch_size, validation_data=(x_test, y_test))
